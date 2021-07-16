0f7e58a3ca9eba77d29fd5514eca7ab4eef1f64c
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-07-16 12:14:08 +0530

    Table controler completed

--- src/controllers/tableController.js
+++ src/controllers/tableController.js
@@ -38,4 +38,192 @@ const addTable=async(req,res)=>{
     }
 }
 
-module.exports={addTable}
+const getTableDetail=async(req,res)=>{
+    try{
+        const id=req.params.id
+        if(!id){
+            return res.status(400).json({
+                success:false,
+                error:'No id detected'
+            })
+        }
+        const table=await Table.findById(id)
+        if(!table){
+            return res.status(400).json({
+                success:false,
+                error:'No such table found'
+            })
+        }
+        res.status(200).json({
+            success:true,
+            data:{
+                _id:table._id,
+                price:table.price,
+                number:table.number,
+                isBooked:table.isBooked
+            }
+        })
+    }catch(e){
+        return res.status(500).json({
+            success:false,
+            error:'Server error'
+        })
+    }
+}
+
+const getTableDetailForAdmin=async(req,res)=>{
+    try{
+        
+        const admin=req.user
+        if(!admin){
+            return res.status(401).json({
+                success:false,
+                error:'Not authorized as admin'
+            })
+        }
+        const id=req.params.id
+        if(!id){
+            return res.status(400).json({
+                success:false,
+                error:'No id detected'
+            })
+        }
+        const table=await Table.findById(id).populate('addedBy')
+        if(!table){
+            return res.status(400).json({
+                success:false,
+                error:'No such table found'
+            })
+        }
+        res.status(200).json({
+            success:true,
+            data:{
+                _id:table._id,
+                price:table.price,
+                number:table.number,
+                isBooked:table.isBooked,
+                addedBy:table.addedBy,
+                bookedBy:table.bookedBy
+            }
+        })
+    }catch(e){
+        return res.status(500).json({
+            success:false,
+            error:'Server error'
+        })
+    }
+}
+
+const getAllTableDetails=async(req,res)=>{
+    try{
+        const tables=await Table.find({}).select(['-bookedBy','-addedBy'])
+        res.status(200).json({
+            success:true,
+            data:tables
+        })
+    }catch(e){
+        return res.status(500).json({
+            success:false,
+            error:'Server error'
+        })
+    }
+}
+
+
+
+const updateTable=async(req,res)=>{
+    try{
+        const {number,price,isBooked,bookedBy}=req.body
+        if(!req.params.id){
+            return req.status(400).json({
+                success:false,
+                error:'No id detected'
+            })
+        }
+        const table=await Table.findById(req.params.id)
+        if(!table){
+            return res.status(404).json({
+                success:false,
+                error:'Table does not exist'
+            })
+        }
+        const admin=req.user
+        if(!admin){
+            return res.status(401).json({
+                success:false,
+                error:'Not authorized as admin'
+            })
+        }
+        if(table){
+            table.number=number||table.number
+            table.price=price||table.price
+            table.isBooked=isBooked||table.isBooked
+            if(isBooked)
+                table.bookedBy=bookedBy||table.bookedBy
+            else
+                table.bookedBy=null
+        }
+        const updatedTable=await table.save()
+        if(updatedTable){
+            res.status(200).json({
+                success:true,
+                data:updatedTable
+            })
+        }else{
+            return res.status(400).json({
+                success:false,
+                error:'Wrong Table data'
+            })
+        }
+    }catch(e){
+        return res.status(500).json({
+            success:false,
+            error:'Server error'
+        })
+    }
+}
+
+const deleteTable=async(req,res)=>{
+    try{
+        const admin=req.user
+        if(!admin){
+            return res.status(401).json({
+                success:false,
+                error:'Not authorized as admin'
+            })
+        }
+        if(!req.params.id){
+            return req.status(400).json({
+                success:false,
+                error:'No id detected'
+            })
+        }
+        const table=await Table.findById(req.params.id)
+        if(!table){
+            return res.status(404).json({
+                success:false,
+                error:'Table does not exist'
+            })
+        }
+        if(table.isBooked){
+            return res.status(404).json({
+                success:false,
+                error:'Table is Booked by someone'
+            })
+        }
+        const deletedTable=await table.remove()
+        if(deletedTable){
+            res.status(200).json({
+                success:true,
+                data:deletedTable
+            })
+        }
+    }catch(e){
+        return res.status(500).json({
+            success:false,
+            error:'Server error'
+        })
+    }
+}
+
+module.exports={addTable,updateTable,getTableDetail,getAllTableDetails,getTableDetailForAdmin,deleteTable}
