769ffeb50dd3b903d2ed032c020701bc21c77db2
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-07-18 16:37:30 +0530

    Server message added

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ const PORT=process.env.PORT||5000
 const adminRoutes=require('./src/adminPanel/adminAPIS')
 const userRoutes=require('./src/userPanel/userAPIS')
 
+app.get('/',(req,res)=>{
+    res.send({
+        message:'Server is running successfully'
+    })
+})
+
 app.use('/api/admin',adminRoutes)
 app.use('/api/user',userRoutes)
+
 
 
 app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`.yellow)})
