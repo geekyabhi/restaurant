c592910c3d56bcf91919ba2704515e72f4c3c2eb
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-07-24 11:22:31 +0530

    :

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const connectDB = require("./src/db/mongoose");
 require("colors");
+require("./src/redis/redisServer");
 const app = express();
 connectDB();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
-const REDIS_PORT = process.env.REDIS_URL || "http://127.0.0.1:6379";
 
 const adminRoutes = require("./src/adminPanel/adminAPIS");
 const userRoutes = require("./src/userPanel/userAPIS");
@@ -21,5 +21,4 @@ app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`.yellow);
-	console.log(`Redis runnning on port ${REDIS_PORT}`.yellow);
 });
--- src/redis/redisServer.js
+++ src/redis/redisServer.js
@@ -2,5 +2,7 @@ const redis = require("redis");
 
 const REDIS_PORT = process.env.REDIS_URL || "http://127.0.0.1:6379";
 const redisClient = redis.createClient(REDIS_PORT);
-
+if (redisClient) {
+	console.log(`Reddis running on port ${REDIS_PORT}`.yellow);
+}
 module.exports = redisClient;
