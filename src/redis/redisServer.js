88e244bd844b889b5d478ebc36a9162a58ad686b
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-07-24 17:06:05 +0530

    REdis lab used

--- src/redis/redisServer.js
+++ src/redis/redisServer.js
@@ -1,8 +1,13 @@
 const redis = require("redis");
 
-const REDIS_PORT = process.env.REDIS_URL || "http://127.0.0.1:6379";
+// const REDIS_PORT = process.env.REDIS_URL || "http://127.0.0.1:6379";
+const REDIS_PORT = {
+	host: process.env.REDIS_HOST,
+	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_PASSWORD,
+};
 const redisClient = redis.createClient(REDIS_PORT);
 if (redisClient) {
-	console.log(`Reddis running on port ${REDIS_PORT}`.yellow);
+	console.log(`Redis running on port ${process.env.REDIS_PORT}`.yellow);
 }
 module.exports = redisClient;
